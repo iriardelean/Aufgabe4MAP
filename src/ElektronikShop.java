4031f08859ac6bd40df5e6b245b5b509a1363b8a
iriardelean <iriardelean33@gmail.com>
2025-10-21 15:18:33 +0300

    3rd & 4th exercises + final commit

--- src/ElektronikShop.java
+++ src/ElektronikShop.java
@@ -31,4 +31,48 @@ public class ElektronikShop {
 
         return maxPreis;
     }
+
+    // Schreiben Sie eine Methode, welche das teuerste USB Laufwerk, das Markus kaufen kann, zurückgibt.
+    public static int getTeuersteUsbInBudget(int[] preise, int budget) {
+        if (preise.length == 0)
+            throw new IllegalArgumentException("Das Array darf nicht null oder leer sein.");
+
+        int maxKaufbar = -1;
+
+        for (int i = 0; i < preise.length; i++) {
+            int preis = preise[i];
+
+            if (preis <= budget)
+                if (preis > maxKaufbar)
+                    maxKaufbar = preis;
+        }
+
+        return maxKaufbar;
+    }
+
+    // Finden Sie, anhand Markus’ Budget und der Preislisten für die Tastaturen und USB-Laufwerke, den maximalen Geldbetrag,
+    // der von Markus ausgegeben wird. Wenn er nicht genug für beide hat, geben Sie stattdessen -1 zurück.
+    // Er kauft nur die zwei benötigten Gegenstände.
+    public static int getMaximalAusgabe(int budget, int[] tastaturen, int[] usb) {
+        if (tastaturen.length == 0)
+            throw new IllegalArgumentException("Das Array darf nicht null oder leer sein.");
+        if (usb.length == 0)
+            throw new IllegalArgumentException("Das Array darf nicht null oder leer sein.");
+
+        int maxAusgabe = -1;
+
+        for (int i = 0; i < tastaturen.length; i++)
+            for (int j = 0; j < usb.length; j++) {
+
+                int tPreis = tastaturen[i];
+                int uPreis = usb[j];
+                int summe = tPreis + uPreis;
+
+                if (summe <= budget)
+                    if (summe > maxAusgabe)
+                        maxAusgabe = summe;
+            }
+
+        return maxAusgabe;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,30 @@ public class Main {
         int[] usbPreise2 = {20, 15, 40, 15};
         System.out.println("Aufgabe 2 - Teuerster Gegenstand aus " + Arrays.toString(tastaturPreise2) + " und " + Arrays.toString(usbPreise2) + ":");
         System.out.println("Ergebnis: " + ElektronikShop.getTeuersteGegenstand(tastaturPreise2, usbPreise2));
+
+        // aufgabe 3
+        int[] usbPreise3 = {15, 45, 20};
+        int budget3 = 30;
+        System.out.println("Aufgabe 3 - Teuerstes USB aus " + Arrays.toString(usbPreise3) + " mit Budget " + budget3 + ":");
+        System.out.println("   Ergebnis: " + ElektronikShop.getTeuersteUsbInBudget(usbPreise3, budget3));
+
+        // aufgabe 4
+        int budget4_1 = 60;
+        int[] tastaturen4_1 = {40, 50, 60};
+        int[] usb4_1 = {8, 12};
+        System.out.println("Aufgabe 4.1 - Maximale Ausgabe (Budget: " + budget4_1 + ", T: " + Arrays.toString(tastaturen4_1) + ", U: " + Arrays.toString(usb4_1) + "):");
+        System.out.println("   Ergebnis: " + ElektronikShop.getMaximalAusgabe(budget4_1, tastaturen4_1, usb4_1));
+
+        int budget4_2 = 60;
+        int[] tastaturen4_2 = {60};
+        int[] usb4_2 = {8, 12};
+        System.out.println("Aufgabe 4.2 - Maximale Ausgabe (Budget: " + budget4_2 + ", T: " + Arrays.toString(tastaturen4_2) + ", U: " + Arrays.toString(usb4_2) + "):");
+        System.out.println("   Ergebnis: " + ElektronikShop.getMaximalAusgabe(budget4_2, tastaturen4_2, usb4_2));
+
+        int budget4_3 = 60;
+        int[] tastaturen4_3 = {40, 60};
+        int[] usb4_3 = {8, 12};
+        System.out.println("Aufgabe 4.3 - Maximale Ausgabe (Budget: " + budget4_3 + ", T: " + Arrays.toString(tastaturen4_3) + ", U: " + Arrays.toString(usb4_3) + "):");
+        System.out.println("   Ergebnis: " + ElektronikShop.getMaximalAusgabe(budget4_3, tastaturen4_3, usb4_3));
     }
 }
